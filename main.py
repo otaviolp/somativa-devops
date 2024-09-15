6ecc3eaefdb417c55acce8649809b1e82cc55a98
otaviolp <otaviolpaiola@gmail.com>
2024-09-15 20:48:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#ATP - DevOps - PUCPR
+#Otavio Luigi Paiola
+
 # Lista de tarefas vazia
 tarefas = []
 
@@ -9,3 +12,9 @@ def menu():
     print("3. Marcar tarefa como concluída")
     print("4. Remover tarefa")
     print("5. Sair")
+
+# Função para adicionar tarefa
+def adicionar_tarefa():
+    tarefa = input("Digite a nova tarefa: ")
+    tarefas.append({"nome": tarefa, "concluida": False})
+    print(f"Tarefa '{tarefa}' adicionada.")
