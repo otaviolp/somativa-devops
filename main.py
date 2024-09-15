0dc506eeba48c9ddf9650b5b3792e0f814096913
otaviolp <otaviolpaiola@gmail.com>
2024-09-15 20:57:27 -0300

    Função Listar Tarefas

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ def adicionar_tarefa():
     tarefa = input("Digite a nova tarefa: ")
     tarefas.append({"nome": tarefa, "concluida": False})
     print(f"Tarefa '{tarefa}' adicionada.")
+
+# Função para listar tarefas
+def listar_tarefas():
+    if not tarefas:
+        print("Nenhuma tarefa na lista.")
+    else:
+        print("\nTarefas:")
+        for i, tarefa in enumerate(tarefas):
+            status = "Concluída" if tarefa["concluida"] else "Pendente"
+            print(f"{i + 1}. {tarefa['nome']} [{status}]")
